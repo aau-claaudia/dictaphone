18bf2303a314235bb39bda640aeb758bd14c51b8
Nikolaj Andersen <nikolajandersen@its.aau.dk>
2025-08-05 15:44:42 +0200

    Fixed unittests to work with the async updates.

--- dictaphone/test_audio_chunk_manager.py
+++ dictaphone/test_audio_chunk_manager.py
@@ -1,13 +1,22 @@
 import os
 import unittest
 from pathlib import Path
+import asyncio
+import functools
+
+def async_test(coro):
+    """A decorator to run async test methods with the standard unittest runner."""
+    @functools.wraps(coro)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(coro(*args, **kwargs))
+    return wrapper
 
 class DummyConsumer:
     """A dummy consumer to capture request re-send messages sent by AudioChunkManager."""
     def __init__(self):
         self.sent_messages = []
 
-    def send_to_client(self, msg):
+    async def send_to_client(self, msg):
         self.sent_messages.append(msg)
 
 class TestAudioChunkManager(unittest.TestCase):
@@ -39,45 +48,49 @@ class TestAudioChunkManager(unittest.TestCase):
         with open(self.output_file, "rb") as f1, open(self.reference_file, "rb") as f2:
             self.assertEqual(f1.read(), f2.read())
 
-    def test_in_order(self):
+    @async_test
+    async def test_in_order(self):
         # 1) All chunks in order
         for idx in range(5):
             data = self.load_chunk(idx)
-            self.manager.add_chunk(self.recording_id, idx, data)
+            await self.manager.add_chunk(self.recording_id, idx, data)
         self.compare_output_to_reference()
 
-    def test_out_of_order(self):
+    @async_test
+    async def test_out_of_order(self):
         # 2) Chunks out of order
         order = [0, 2, 1, 4, 3]
         for idx in order:
             data = self.load_chunk(idx)
-            self.manager.add_chunk(self.recording_id, idx, data)
+            await self.manager.add_chunk(self.recording_id, idx, data)
         # Manager should request resend for chunk 1
         self.assertTrue(any(msg.get('chunk_index') == 1 for msg in self.consumer.sent_messages))
         self.compare_output_to_reference()
 
-    def test_missing_and_resend(self):
+    @async_test
+    async def test_missing_and_resend(self):
         # 3) Leave out chunk 2, then resend it
         for idx in [0, 1, 3, 4]:
             data = self.load_chunk(idx)
-            self.manager.add_chunk(self.recording_id, idx, data)
+            await self.manager.add_chunk(self.recording_id, idx, data)
         # Manager should request resend for chunk 2
         self.assertTrue(any(msg.get('chunk_index') == 2 for msg in self.consumer.sent_messages))
         # Now supply chunk 2
         data = self.load_chunk(2)
-        self.manager.add_chunk(self.recording_id, 2, data)
+        await self.manager.add_chunk(self.recording_id, 2, data)
         self.compare_output_to_reference()
 
-    def test_first_chunk_missing(self):
+    @async_test
+    async def test_first_chunk_missing(self):
         # 4) Start with chunk 1, then supply chunk 0 after request
         for idx in [1, 2, 3, 4]:
             data = self.load_chunk(idx)
-            self.manager.add_chunk(self.recording_id, idx, data)
+            await self.manager.add_chunk(self.recording_id, idx, data)
         # Manager should request resend for chunk 0
         self.assertTrue(any(msg.get('chunk_index') == 0 for msg in self.consumer.sent_messages))
         # Now supply chunk 0
         data = self.load_chunk(0)
-        self.manager.add_chunk(self.recording_id, 0, data)
+        await self.manager.add_chunk(self.recording_id, 0, data)
         self.compare_output_to_reference()
 
 if __name__ == "__main__":
