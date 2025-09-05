22715401bfa27283771bd279148f6ff3ec2f6c11
Nikolaj Andersen <nikolajandersen@its.aau.dk>
2025-09-05 13:14:34 +0200

    Vite config.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,22 +3,22 @@ import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  //plugins: [react()],
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
+  //plugins: [react(), basicSsl()],
   server: {
     host: true, // make development server accessible from the wireless network, for testing on mobile device
     proxy: {
       // Proxy WebSocket connections to the Daphne server
       '/ws': {
-        //target: 'ws://localhost:8000',
-        target: 'ws://10.49.223.156:8000',
+        target: 'ws://localhost:8000',
+        //target: 'ws://10.49.223.156:8000',
         ws: true, // This is crucial for WebSocket proxying
       },
       // Proxy media file requests to the Django/Daphne server.
       // Match any URL that contains '/media/RECORDINGS/'
       '^.*(/media/RECORDINGS/.*)$': {
-        //target: 'http://localhost:8000',
-        target: 'http://10.49.223.156:8000',
+        target: 'http://localhost:8000',
+        //target: 'http://10.49.223.156:8000',
         changeOrigin: true,
       },
     },
