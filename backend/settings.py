8b25073bb46cfd2a96b8ac4fb05f6660434bed4c
Nikolaj Andersen <nikolajandersen@its.aau.dk>
2026-06-08 13:37:15 +0200

    Updated the redis host to use IP address rather than localhost.

--- backend/settings.py
+++ backend/settings.py
@@ -153,8 +153,8 @@ CORS_ALLOW_CREDENTIALS = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Add Celery configuration
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 
 # see https://channels.readthedocs.io/en/latest/deploying.html
 CHANNEL_LAYERS = {
@@ -162,7 +162,7 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             # Using different database number (1) to keep data separate from celery data (0)
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379/1')],
+            "hosts": ['redis://127.0.0.1:6379/1'],
         },
     },
 }
