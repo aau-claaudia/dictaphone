6514af5704faa9926b90071b3cc332b65f09146b
Nikolaj Andersen <nikolajandersen@its.aau.dk>
2026-06-09 11:03:30 +0200

    Debugging redis ws socket connectivity issue.

--- backend/settings.py
+++ backend/settings.py
@@ -153,8 +153,8 @@ CORS_ALLOW_CREDENTIALS = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Add Celery configuration
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/1'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/1'
 
 # see https://channels.readthedocs.io/en/latest/deploying.html
 CHANNEL_LAYERS = {
@@ -163,7 +163,7 @@ CHANNEL_LAYERS = {
         'CONFIG': {
             # Using different database number (1) to keep data separate from celery data (0)
             "hosts": [{
-                "address": "redis://127.0.0.1:6379/1",
+                "address": "redis://127.0.0.1:6379/0",
                 "socket_timeout": 5,
                 "socket_connect_timeout": 5,
                 "retry_on_timeout": True,
